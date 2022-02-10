bf37685105ead27b5aba3b7573d9a072290c90b0
DeathByDusk <elijahkanellakis@gmail.com>
2022-02-10 11:26:52 -0600

    added authentication context

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -16,6 +16,17 @@ const resolvers = {
         .populate('friends')
         .populate('thoughts');
     },
+    me: async (parent, args, context) => {
+      if (context.user) {const userData = await User.findOne({ _id: context.user._id })
+        .select('-__v -password')
+        .populate('thoughts')
+        .populate('friends');
+
+        return userData;
+      }
+      
+      throw new AuthenticationError('Not logged in');
+    },
     thoughts: async (parent, { username }) => {
       const params = username ? { username } : {};
       return Thought.find(params).sort({ createdAt: -1 });
